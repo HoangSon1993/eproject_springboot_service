e5ebc5ec339f2cfc4ce9042fcde665ef14a49d42
HoangSon <son.lyhoang2014@gmail.com>
2024-07-14 09:21:13 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# eproject_springboot_service
---
+++ src/test/java/com/sontung/eproject_spring_service/EprojectSpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sontung.eproject_spring_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EprojectSpringServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
